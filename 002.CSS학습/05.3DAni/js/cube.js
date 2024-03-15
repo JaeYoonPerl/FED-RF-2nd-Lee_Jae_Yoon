3bfce44e547e0e5647d85f9798cea8cf3032e01a
JaeYoonLee <a7750430@naver.com>
2024-03-15 14:55:57 +0900

    dd

--- 002.CSS학습/05.3DAni/js/cube.js
+++ 002.CSS학습/05.3DAni/js/cube.js
@@ -27,6 +27,12 @@ btngo.onclick = function(){
     // 3. 변경내용: 큐브에 클래스on 을 없으면 넣고 있으면 제거한다
     // -> 미리 셋팅된 애니 작동/멈춤됨
     cube.classList.toggle('on');
+
+    // 큐브 거리변경 변수셋팅은 
+    // html 요소에 클래스 on을 넣기/빼기하면 적용된다.
+    document.querySelector('html')
+    .classList.toggle('on');
+
     // classList는 요소의 클래스만 전문적으로 다뤄주는 JS 내장 객체다.
     // 메서드로 add()-넣기, remove()-지우기 , toggle()-넣기/지우기
     // 4. 버튼 글자 변경하가
@@ -38,7 +44,7 @@ btngo.onclick = function(){
 
     //if문으로 글자변경제어하기
     // if(this.innerText == '돌아!'){
-    //     this.innerText = '멈춰!'; 
+    //     this.innerText = '멈춰!';   
     // }else{
     //     this.innerText = '돌아!';
     // }
