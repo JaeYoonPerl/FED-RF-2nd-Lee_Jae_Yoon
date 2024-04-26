272e6572c24382ccf31a0a0105e31288b7c73308
JaeYoonLee <a7750430@naver.com>
2024-04-26 17:48:03 +0900

    Update drag_slide.js

--- 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/drag_slide.js
+++ 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/drag_slide.js
@@ -393,22 +393,40 @@ function slideFn(selEl) {
       let currList = mFn.qsaEl(dtg,"li");
       // (2) 리스트 길이(개수)
       let listLength = currList.length;
-      // (3) 리스트 한개당 크기(li 가로 크기)
+      // (3) 리스트 한개당 크기(li가로크기)
       let oneSize = currList[0].offsetWidth;
+      // (4) 마지막위치 한계값 left
+      // -> 히든박스width -전체 슬라이드 width
+      // 전체 슬라이드 width = li한개당 width * 슬라이드개수
+      let limitSize = 
+      selEl.offsetWidth - (oneSize * listLength);
+      console.log('마지막한계Left:',limitSize);
 
-      // 4-1. 맨앞에서 튕기기 
+      // 4-1. 맨앞에서 튕기기
       if(parseInt(dtg.style.left)>0){
         // 약간의 시간간격으로 조금 간후 튕겨서 돌아오는 효과
-        setTimeout(()=>{
-          // left값 0
+        setTimeout(() => {
+          // left 값 0
           dtg.style.left = "0";
           // 마지막 위치값 0
-          lastX=0;
+          lastX = 0;
+        }, 200);
+      } ////// if /////////
 
-        },200)
-      } /// if
+      // 4-2. 맨뒤에서 튕기기
+      if(parseInt(dtg.style.left)<limitSize){
+        // 약간의 시간간격으로 조금 간후 튕겨서 돌아오는 효과
+        setTimeout(() => {
+          // left 값 0
+          dtg.style.left = limitSize+"px";
+          // 마지막 위치값 0
+          lastX = limitSize;
+        }, 200);
+      } ////// if /////////
 
-    } //// if ////////
+
+
+    } /////// if ////////
 
     // 드래그 중(dragSts===true)일때는 주먹손(grabbing),
     // 드래그아닐때(dragSts===false) 편손(grab)
