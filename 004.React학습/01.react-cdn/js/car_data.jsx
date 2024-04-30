0b3cd64ab651188d6e6ea536b30aa6314e9cb5d5
JaeYoonLee <a7750430@naver.com>
2024-04-30 16:20:03 +0900

    dd

--- 004.React학습/01.react-cdn/js/car_data.jsx
+++ 004.React학습/01.react-cdn/js/car_data.jsx
@@ -1,7 +1,7 @@
 // 자동차 정보파일 ////
 const carInfo = 
 {
-    "기아레이":[
+    기아레이:[
         {
             color:"라잇블루",
             model:"2023년형",
@@ -18,11 +18,11 @@ const carInfo =
             opt:{filter:'hue-rotate(215deg)'}
         },
     ],
-    "현대제네시스":[
+    현대제네시스:[
         {
             color:"스틸그레이",
             model:"2024년형",
-            opt:{filter:'hue-rotate(0deg)'}
+            opt:{filter:'grayscale(1)'}
         },
         {
             color:"세단블랙",
@@ -30,12 +30,18 @@ const carInfo =
             opt:{filter:'hue-rotate(109deg)',transform:'rotateY(180deg)'}
         },
         {
-            color:"라잇블루",
+            color:"라잇그린",
             model:"2024년형",
             opt:{filter:'hue-rotate(215deg)'}
         },
     ],
+};
+
+const carImage ={
+    "기아레이":"ray.png",
+    "현대제네시스":"genesis.png",
 }
 
 
-export {carInfo};
+
+export {carInfo, carImage};
