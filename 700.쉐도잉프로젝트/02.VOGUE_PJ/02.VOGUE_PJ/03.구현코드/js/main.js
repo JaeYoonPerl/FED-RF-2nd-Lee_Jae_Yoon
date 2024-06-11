d8e93b89a26b82a5e8bb51f09ebad525ad483927
JaeYoonLee <a7750430@naver.com>
2024-06-11 15:20:27 +0900

    qwe

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/02.VOGUE_PJ/03.구현코드/js/main.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/02.VOGUE_PJ/03.구현코드/js/main.js
@@ -14,95 +14,60 @@ import Gallery from "./components/Gallery";
 import Login from "./components/Login";
 // 회원가입 모듈 불러오기
 import Member from "./components/Member";
-// 부드러운 스크롤 불러오기
-import { scrolled, setPos } from "./smoothScroll24";
 
 // [1] 메인 페이지 전체 레이아웃 로딩 컴포넌트 ///
 function Layout() {
-    // 상태관리변수 설정구역 ////////
-    // [1] 메뉴 변경 상태변수
-    const [menu, setMenu] = React.useState("home");
+  // 상태관리변수 설정구역 ////////
+  // [1] 메뉴 변경 상태변수
+  const [menu, setMenu] = React.useState("home");
 
-    // 화면 랜더링 직전에 CSS로딩 변경하기 /////
-    React.useLayoutEffect(() => {
-        // menu 상태변수에 의존시킨다!
-        // 메인 css 대상요소 : #main-css
-        document.querySelector("#main-css").href = menu == "home" ? "./css/main.css" : menu == "gallery" ? "./css/gallery.css" : menu == "login" ? "./css/login.css" : menu == "member" ? "./css/member.css" : "./css/items.css";
-        // menu값이 "home"인 경우 main.css를 로딩하고
-        // menu값이 "gallery"인 경우 gallery.css를 로딩하고
-        // menu값이 "login"인 경우 login.css를 로딩하고
-        // menu값이 "member"인 경우 member.css를 로딩하고
-        // 기타 메뉴인 경우 items.css를 로딩한다!
+  // 화면 랜더링 직전에 CSS로딩 변경하기 /////
+  React.useLayoutEffect(() => {
+    // menu 상태변수에 의존시킨다!
+    // 메인 css 대상요소 : #main-css
+    document.querySelector("#main-css").href =
+      menu == "home"
+        ? "./css/main.css"
+        : menu == "gallery"
+        ? "./css/gallery.css"
+        : menu == "login"
+        ? "./css/login.css"
+        : menu == "member"
+        ? "./css/member.css"
+        : "./css/items.css";
+    // menu값이 "home"인 경우 main.css를 로딩하고
+    // menu값이 "gallery"인 경우 gallery.css를 로딩하고
+    // menu값이 "login"인 경우 login.css를 로딩하고
+    // menu값이 "member"인 경우 member.css를 로딩하고
+    // 기타 메뉴인 경우 items.css를 로딩한다!
 
-        // 페이지 최상단이동코드
-        window.scrollTo(0, 0);
+    // 페이지 최상단이동코드
+    window.scrollTo(0, 0);
 
-        ////////////////////////////////////////////////////////
-        // [ 이벤트의 해제는 removeEventListener()를 사용한다!!! ]
-        // 부드러운 스크롤은 "home"에서만 적용함!
-        if (menu == "home") document.addEventListener("wheel", scrolled, { passive: false });
-        // "home"이 아닌경우는 모두 이벤트를 해재한다!
-        else document.removeEventListener("wheel", scrolled, { passive: false });
-        /////////////////////////////////////////////////////////
+    
+  }, [menu]);
 
-        // 슬림적용 대상: #top-area
-        const topMenu = document.querySelector("#top-area");
-        // 상단이동 버튼 대상 : .tbtn
-        const tbtn = document.querySelector(".tbtn");
-        // 상단 이동 기능
-        tbtn.onclick = (e) => {
-            // 기본이동막기
-            e.preventDefault();
-            // 상단 이동하기 : 부드러운스크롤 위치값 업데이트
-            setPos(0);
-            // 위치값 이동하기
-            window.scrollTo(0,0);
-        }; //// click
-
-        // 슬림메뉴 적용하기 : "home"에서만 적용
-        const chkSlim = (e) => {
-            e.preventDefault();
-
-            // 메뉴 home 일때만 적용
-            if (menu == "home") {
-                // 스크롤 위치값 구하기
-                let scTop = window.scrollY;
-                console.log("슬림적용!!!", scTop);
-
-                // 슬림메뉴 적용
-                if (scTop > 200) topMenu.classList.add("on");
-                else topMenu.classList.remove("on");
-
-                // 상단이동버튼 적용
-                if (scTop > 300) tbtn.classList.add("on");
-                else tbtn.classList.remove("on");
-            } /// 메뉴  "home"일때만 적용
-        }; //////// chkSlim 함수 /////////
-
-        // 스크롤 이벤트 적용하기 : scroll이벤트
-        // "home"에서만 적용하기
-        if (menu == "home") {
-            console.log("홈이야~!");
-            setPos(0);
-            window.addEventListener("scroll", chkSlim);
-        } else {
-            console.log("서브야~!");
-            setPos(0);
-            window.removeEventListener("scroll", chkSlim);
-        }
-    }, [menu]);
-
-    // 코드 리턴구역 ////////////
-    return (
-        <React.Fragment>
-            {/* 1. 상단영역 컴포넌트 */}
-            <TopArea changeMenu={setMenu} />
-            {/* 2. 메인영역 컴포넌트 */}
-            {menu == "home" ? <MainArea /> : menu == "gallery" ? <Gallery /> : menu == "login" ? <Login changeMenu={setMenu} /> : menu == "member" ? <Member changeMenu={setMenu} /> : <ItemsArea catName={menu} />}
-            {/* 3. 하단영역 컴포넌트 */}
-            <FooterArea />
-        </React.Fragment>
-    );
+  // 코드 리턴구역 ////////////
+  return (
+    <React.Fragment>
+      {/* 1. 상단영역 컴포넌트 */}
+      <TopArea changeMenu={setMenu} />
+      {/* 2. 메인영역 컴포넌트 */}
+      {menu == "home" ? (
+        <MainArea />
+      ) : menu == "gallery" ? (
+        <Gallery />
+      ) : menu == "login" ? (
+        <Login changeMenu={setMenu} />
+      ) : menu == "member" ? (
+        <Member changeMenu={setMenu} />
+      ) : (
+        <ItemsArea catName={menu} />
+      )}
+      {/* 3. 하단영역 컴포넌트 */}
+      <FooterArea />
+    </React.Fragment>
+  );
 } ///////// Layout 컴포넌트 /////////
 
 // 메인 페이지 전체 출력하기
--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Main.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Main.jsx
@@ -3,6 +3,7 @@
 import Banner from "../modules/Banner";
 import SecIntro from "../modules/SecIntro";
 import VidIntro from "../modules/VidIntro";
+import VidSwipe from "../modules/VidSwipe";
 
 export default function Main(){
     // 코드 리턴구역 ////
@@ -18,6 +19,8 @@ export default function Main(){
            {/* 3. 비디오 소개 컴포넌트 
            catName 카테고리명, clsName 배경색 넣을 클래스(on/off) */}
            <VidIntro catName="main" clsName="off"/>
+           {/* 4. 비디오스와이프 컴포넌트 */}
+           <VidSwipe catName="main"/>
         </>
     );
 } /////// Main /////
