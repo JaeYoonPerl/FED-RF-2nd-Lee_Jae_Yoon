1f20a626da21332f31d13fc44168abd691dfbb3d
JaeYoonLee <a7750430@naver.com>
2024-06-19 10:22:43 +0900

    Update MainArea.jsx

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/layout/MainArea.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/layout/MainArea.jsx
@@ -1,12 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import MainCont from '../pages/MainCont';
 
 function MainArea(props) {
-    
+    // 상태관리 변수 셋팅
+    // 1. 페이지 변경 상태 변수
+    const [pgName,setPgName] = useState("main");
+
+    // 코드 리턴 구역
     return (
-        <div>
+        <>
             <MainCont />
-        </div>
+        </>
     );
 }
 
