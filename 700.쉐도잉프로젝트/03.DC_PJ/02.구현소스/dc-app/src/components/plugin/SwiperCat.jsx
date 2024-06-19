960df86bdec5b33b0e62541e084aafec55045187
JaeYoonLee <a7750430@naver.com>
2024-06-19 16:19:10 +0900

    Update SwiperCat.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/plugin/SwiperCat.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/plugin/SwiperCat.jsx
@@ -57,25 +57,26 @@ export function SwiperCat() {
                         // 숫자형 변환해줌 Number(변수)
                         Number(v.idx) <= 7 && (
                             <SwiperSlide key={i}>
-                                <Link to="/detail"
-                                // state로 3가지 값을 넘겨준다
-                                state={{
-                                    cname:v.cname, // 캐릭터이름
-                                    cdesc:v.cdesc, // 캐릭터설명
-                                    facts:v.facts, // 캐릭터상세
-                                }}
+                                <Link
+                                    to="/detail"
+                                    // state로 3가지 값을 넘겨준다
+                                    state={{
+                                        cname: v.cname, // 캐릭터이름
+                                        cdesc: v.cdesc, // 캐릭터설명
+                                        facts: v.facts, // 캐릭터상세
+                                    }}
                                 >
-                                <section className="sw-inbox2">
-                                    {/* 캐릭터이미지영역 */}
-                                    <div className="cat-img2">
-                                        <img src={v.tmsrc} alt={v.cname} />
-                                    </div>
-                                    {/* 캐릭터타이틀영역 */}
-                                    <div className="cat-tit2">
-                                        <h3>{v.cname}</h3>
-                                    </div>
-                                </section>
-                                    </Link> 
+                                    <section className="sw-inbox2">
+                                        {/* 캐릭터이미지영역 */}
+                                        <div className="cat-img2">
+                                            <img src={v.tmsrc} alt={v.cname} />
+                                        </div>
+                                        {/* 캐릭터타이틀영역 */}
+                                        <div className="cat-tit2">
+                                            <h3>{v.cname}</h3>
+                                        </div>
+                                    </section>
+                                </Link>
                             </SwiperSlide>
                         )
                 )}
