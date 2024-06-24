42ccef4cb17e26bc62ca7359557755ea8cd85767
JaeYoonLee <a7750430@naver.com>
2024-06-24 10:42:30 +0900

    dd

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Movies.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Movies.jsx
@@ -1,6 +1,7 @@
 // 무비스 페이지 컴포넌트 ///
 import Banner from "../modules/Banner";
 import VidIntro from "../modules/VidIntro";
+import VidSwipe from "../modules/VidSwipe";
 export default function Movies() {
     // 코드 리턴구역 ////
     return (
@@ -9,6 +10,9 @@ export default function Movies() {
             <Banner catName="MOVIES" />
             {/* 1. 비디오소개 컴포넌트 */}
             <VidIntro catName="MOVIES" clsName="on"/>
+
+            {/* 4. 비디오스와이프 컴포넌트 */}
+           <VidSwipe catName="movies"/>
             
         </>
     );
--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/index.js
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/index.js
@@ -56,7 +56,9 @@ import SearchPage from "./components/pages/SearchPage";
 export default function MainComponent() {
     return (
         // 라우터 루트로 라우터 구성시작
-        <BrowserRouter>
+        // basename 속성은 package.json에 "homepage"속성값을 읽어옴
+        // (읽는 방범은 process.env.PUBLIC_URL )
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Routes>
                 {/* 중요! 레이아웃 컴포넌트를 루트로 설정 
                 루트 Route는 홀로 닫지말고 반드시 다른 하위
